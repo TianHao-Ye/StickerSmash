939f7b9efa752641e3ad4fd83274e608e6ecf313
Tianhao Ye <yetianhao990225@gmail.com>
2024-05-09 15:32:00 +1000

    final product

--- components/Button.js
+++ components/Button.js
@@ -1,13 +1,18 @@
-import { StyleSheet, View, Pressable, Text } from 'react-native';
-import FontAwesome from "@expo/vector-icons/FontAwesome";
+import { StyleSheet, View, Pressable, Text } from 'react-native'
+import FontAwesome from '@expo/vector-icons/FontAwesome'
 
-export default function Button({ label, theme }) {
-  if (theme === "primary") {
+export default function Button({ label, theme, onPress }) {
+  if (theme === 'primary') {
     return (
-      <View style={[styles.buttonContainer, { borderWidth: 4, borderColor: "#ffd33d", borderRadius: 18 }]}>
+      <View
+        style={[
+          styles.buttonContainer,
+          { borderWidth: 4, borderColor: '#ffd33d', borderRadius: 18 },
+        ]}
+      >
         <Pressable
-          style={[styles.button, { backgroundColor: "#fff" }]}
-          onPress={() => alert('You pressed a button1.')}
+          style={[styles.button, { backgroundColor: '#fff' }]}
+          onPress={onPress}
         >
           <FontAwesome
             name="picture-o"
@@ -15,19 +20,21 @@ export default function Button({ label, theme }) {
             color="#25292e"
             style={styles.buttonIcon}
           />
-          <Text style={[styles.buttonLabel, { color: "#25292e" }]}>{label}</Text>
+          <Text style={[styles.buttonLabel, { color: '#25292e' }]}>
+            {label}
+          </Text>
         </Pressable>
       </View>
-    );
+    )
   }
 
   return (
     <View style={styles.buttonContainer}>
-        <Pressable style={styles.button} onPress={() => alert('You pressed a button2.')}>
-          <Text style={styles.buttonLabel}>{label}</Text>
-        </Pressable>
-      </View>
-  );
+      <Pressable style={styles.button} onPress={onPress}>
+        <Text style={styles.buttonLabel}>{label}</Text>
+      </Pressable>
+    </View>
+  )
 }
 
 const styles = StyleSheet.create({
@@ -54,4 +61,4 @@ const styles = StyleSheet.create({
     color: '#fff',
     fontSize: 16,
   },
-});
+})
